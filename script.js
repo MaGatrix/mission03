b354aa71d7f598eaaaa41c84b8c83ad8136b9a69
ma trix <113900547+MaGatrix@users.noreply.github.com>
2024-06-19 09:36:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const caixaPrincipal = document.querySelector (".caixa-principal");
+const caixaPerguntas = document.querySelector (".caixa-perguntas");
+const caixaAlternativas = document.querySelector (".caixa-alternativa");
+const caixaResultado = document.querySelector (".caixa-resultado");
+const textoResultado = document.querySelector (".texto-resultado");
+
+const perguntas = [
+    {
+    enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+    alternativas: [
+        "Isso é assustador!", 
+        "Isso é maravilhoso!"
+    ]
+},
+{
+    enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre essa tecnologia. No fim de uma aula ela pede que vocẽ escreva um trabalho sobre o uso de tecnologia em sala de aula. Qual atitude você toma? ",
+    alternativas: [
+        "Utilizar ferramentas de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e expplique em uma linguagem que facilite o entendimento. ", 
+        "Escreve o trabalho com base nas conversas que teve com colega, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+    ]
+},
+{
+    enunciado: "Pergunta 2",
+    alternativas: [
+        "Alternativa 1", 
+        "Alternativa 2"
+    ]
+},
+{
+    enunciado: "Pergunta 2",
+    alternativas: [
+        "Alternativa 1", 
+        "Alternativa 2"
+    ]
+},
+
+
+
+
+
+
+
+    "Pergunta 2" 
+]
